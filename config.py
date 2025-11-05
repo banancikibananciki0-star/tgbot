8bda9fc3fe2292772da32bd13218240645049f64
banancikibananciki0-star <banancikibananciki0@gmail.com>
2025-11-05 18:49:06 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Конфигурация бота
+BOT_TOKEN = "8479343356:AAH53xFW45ZL_BFOiwFCxdD9ylKchEgCgiI"  # Замените на реальный токен!
+ADMIN_PASSWORD = "123321"  # Дефолтный пароль
+XRAY_CONFIG_PATH = "/etc/xray/config.json"
+DB_PATH = "xray_bot.db"
+
+# Настройки по умолчанию для новых пользователей
+DEFAULT_PORT = 443
+DEFAULT_PROTOCOL = "vmess"  # Или "vless"
