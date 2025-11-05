8c0702fe43e94599bc3ad6a73cc69da67e96661a
banancikibananciki0-star <banancikibananciki0@gmail.com>
2025-11-05 18:48:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Конфигурация бота
-BOT_TOKEN = "ВАШ_ТОКЕН_ОТ_BOTFATHER"  # Замените на реальный токен!
-ADMIN_PASSWORD = "123321"  # Дефолтный пароль
-XRAY_CONFIG_PATH = "/etc/xray/config.json"
-DB_PATH = "xray_bot.db"
-
-# Настройки по умолчанию для новых пользователей
-DEFAULT_PORT = 443
-DEFAULT_PROTOCOL = "vless"  # Или "vless"
